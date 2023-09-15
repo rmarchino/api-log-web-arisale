ecfda0638fa76656077c06e4378d111806037cfc
Roy <roy.cerdan@arisale.com.pe>
2023-09-15 16:26:25 -0500

    feat: add clicks

--- components/ui/Header.vue
+++ components/ui/Header.vue
@@ -1,42 +1,27 @@
 <template>
-    <!-- Header -->
-    <v-app-bar fixed app color="grey lighten-1">
+  <v-app-bar dark fixed app color="grey lighten-1">
       <v-app-bar-nav-icon @click="drawer = !drawer" />
-      <v-card></v-card>
       <v-list-item-avatar width="120px" height="90px">
-        <v-img
-          src="https://ari-s3bucket.s3.amazonaws.com/assets/logo/arisale_logotipo.png"
-        ></v-img>
+          <v-img
+              src="https://ari-s3bucket.s3.amazonaws.com/assets/logo/arisale_logotipo.png"
+          ></v-img>
       </v-list-item-avatar>
-      
-      <!-- Tabs -->
+  
       <template v-slot:extension>
-        <v-tabs align-with-title>
-          <v-tab>Exceptions</v-tab>
-          <v-tab>Pinpad Events</v-tab>
-          <v-tab>Web services</v-tab>
-        </v-tabs>
+          <v-tabs align-with-title>
+              <v-tab>Exceptions</v-tab>
+              <v-tab>Pinpad Events</v-tab>
+              <v-tab>Web services</v-tab>
+          </v-tabs>
       </template>
-    </v-app-bar>
+  </v-app-bar>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            tabs: [
-                {
-                    name: 'Web Services'
-                },
-                {
-                    name: 'Pinpad Events'
-                },
-                {
-                    name: 'Exceptions'
-                }
-            ],
-            drawer: false
-        }
-    }
+  props: ['drawer'],
+  methods: {
+      // Add your methods here if any
+  }
 }
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,7 @@
 <template>
   <v-app>
     <!-- Header -->
-    <v-app-bar dark fixed app color="grey lighten-1">
-      <!-- <v-app-bar-nav-icon @click="drawer = !drawer" />
-      <v-list-item-avatar width="120px" height="90px">
-        <v-img
-          src="https://ari-s3bucket.s3.amazonaws.com/assets/logo/arisale_logotipo.png"
-        ></v-img>
-      </v-list-item-avatar> -->
-
-      <!-- Tabs -->
-      <!-- <template v-slot:extension>
-        <v-tabs align-with-title>
-          <v-tab>Exceptions</v-tab>
-          <v-tab>Pinpad Events</v-tab>
-          <v-tab>Web services</v-tab>
-        </v-tabs>
-      </template> -->
-      <Header />
-    </v-app-bar>
+    <Header :drawer="drawer" @click.native="drawer = !drawer" />
 
     <v-main>
       <v-container>
